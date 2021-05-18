fdd4a19795ee6255fe332d1a51049c77a26ec9c4
marcus <marcus.longoria12@gmail.com>
2021-05-18 02:40:37 -0500

    commit tree 89df17d75e3d1c6a6f87fe965150b811ceddf5d0

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+function returnFirstTwoDrivers(drivers) {
+    return drivers.slice(0, 2);
+}
+
+function returnLastTwoDrivers(drivers) {
+    return drivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiyValue) {
+    return function (value) {
+        return multiyValue * value;
+    };
+}
+
+function fareDoubler(createFareMultiplier) {
+    return createFareMultiplier * 2;
+}
+
+function fareTripler(createFareMultiplier) {
+    return createFareMultiplier * 3;
+}
+
+function selectDifferentDrivers(drivers, returnDrivers) {
+    return returnDrivers(drivers);
+}
